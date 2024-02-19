22dd3c8b5b08c367a9e2d93e972c8df72da46989
Anshi Paul <anshipaul@outlook.com>
2024-02-19 18:09:02 -0500

    Adding live video compatibility

--- detector.py
+++ detector.py
@@ -1,12 +1,34 @@
-from imageai.Detection import ObjectDetection  
+import cv2
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import pyttsx3
 
-detector = ObjectDetection() 
-model_path = "./models/yolo-tiny.h5"
-input_path = "./input/1.jpg"
-output_path = "./output/newimage.jpg" 
-detector.setModelTypeAsTinyYOLOv3() 
-detector.setModelPath(model_path) 
-detector.loadModel() 
-detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
-for eachItem in detection:
-    print(eachItem["name"] , " : ", eachItem["percentage_probability"])  
+video = cv2.VideoCapture(0)
+
+labels = []
+
+while True:
+     ret, frame = video.read()
+     bbox, label, conf = cv.detect_common_objects(frame)
+     output_image = draw_bbox(frame, bbox, label, conf)
+     
+     cv2.imshow("Household Object Detection", output_image)
+     
+     for item in label:
+          if item in labels:
+                pass
+          labels.append(item)
+     
+     if cv2.waitKey(1) & 0xFF == ord("q"):
+          break
+
+i = 0
+new_sentence = []
+for label in labels:
+     if i==0:
+          new_sentence.append(f"I found a {label}, and, ")
+     else:
+          new_sentence.append(f"a {label}")
+     i+=1
+     
+print(" ".join(new_sentence))
